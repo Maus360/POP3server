322b5b9caad9f611611c9991ff4f7f8125f859b8
Elena Kulinich <elmatthw8@gmail.com>
2019-02-02 09:49:13 +0300

    added some sql queries

--- src/main/java/Database.java
+++ src/main/java/Database.java
@@ -11,6 +11,21 @@ public class Database {
     private PreparedStatement query;
     private static Database instance = null;
 
+    /*SQL queries*/
+    private static final String QUERY_USER_EXISTS = "SELECT `email` FROM `user_credentials` WHERE `email` = ?";
+    private static final String QUERY_PASSWORD_CORRECT = "SELECT `password` FROM `user_credentials` WHERE `email` = ?, " +
+            "`password` = ?";
+    private static final String QUERY_NUM_OF_ALL_MAILS = "SELECT COUNT(*) FROM `mail` NATURAL JOIN " +
+            "`user_credentials_id` WHERE `id` = ?";
+    private static final String QUERY_NUM_OF_MARKED_MAILS = "SELECT COUNT(*) FROM `mail` NATURAL JOIN " +
+            "`user_credentials_id` WHERE `id` = ?";
+    private static final String QUERY_MAILDROP_SIZE = "SELECT SUM(LENGTH(content)) AS `mailDropSize` FROM `mail` " +
+            "INNER JOIN `user_credentials` ON `mail`.`user_credentials_id` = `user_credentials`.`id` " +
+            "WHERE `email` = ? AND `markedForDeletion` = 0";
+    private static final String QUERY_DELETE_MARKED_MAILS = "DELETE `m_mail` FROM `mail` AS `m_mail` " +
+            "WHERE `markedForDeletion` = 1";
+
+
     public Database() {
 
         try {
@@ -47,14 +62,37 @@ public class Database {
         return connection;
     }
 
-    public int myClass(int a, int b){
-        return a - b;
-    }
-
     public static synchronized Database getInstance() {
         if (instance == null) {
             instance = new Database();
         }
         return instance;
+    }
+
+    public boolean userExists(String email){
+        try {
+            query = connection.prepareStatement(QUERY_USER_EXISTS);
+            query.setString(1, email);
+            return query.executeQuery().next();
+        } catch (SQLException e) {
+            System.out.println("User does not exist");
+            System.out.println(e.getMessage());
+        }
+        return false;
+    }
+
+    public boolean passwordCorrect(String email, String password){
+        if (userExists(email)){
+            try {
+                query = connection.prepareStatement(QUERY_PASSWORD_CORRECT);
+                query.setString(1, email);
+                query.setString(2, password);
+                return query.executeQuery().next();
+            } catch (SQLException e) {
+                System.out.println("Password is not correct");
+                System.out.println(e.getMessage());
+            }
+        }
+        return false;
     }
 }
