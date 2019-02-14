a7947cf23c8afdc3a3149e6cc4452f23449f62c6
unknown <elmatthw8@gmail.com>
2019-02-14 23:08:11 +0300

    added some tests

--- src/test/java/TestDatabase.java
+++ src/test/java/TestDatabase.java
@@ -2,19 +2,37 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-public class TestDatabase {
-    @Test
-    public void testconnection(){
-        Database database = new Database();
+import java.sql.SQLException;
 
+public class TestDatabase {
+    private Database database = new Database();
+    @Test(expected = SQLException.class)
+    public void testUserNotExists(){
+        database.userExists("nonexistent@mail.com");
+    }
+
+    @Test
+    public void testUserExists(){
         assertEquals(true, database.userExists("admin@mail.com"));
         assertEquals(false, database.userExists("chel@mail.com"));
     }
 
     @Test
     public void testGetMailDropSize(){
-        Database database = new Database();
-
         assertEquals(81, database.getMaildropSize("bob@mail.com"));
     }
+
+    @Test
+    public void testPasswordCorrect(){
+        assertEquals(true, database.passwordCorrect("bob@mail.com", "hellobob"));
+        assertEquals(false, database.passwordCorrect("admin@mail.com", "admin"));
+    }
+
+    @Test
+    public void testGetNumberOfMessages(){
+        assertEquals(1, database.getNumberOfMessages("bob@mail.com", false));
+    }
+
+
+
 }
